29784e0cf03e1c982203d1dbe3901956537ef9a2
coDev <jh900127@gmail.com>
2022-12-18 06:17:59 +0900

    MAC 세팅 설정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -6,7 +6,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   logging: true,
   port: 3306,
   username: 'root',
-  password: 'Beacon0701!',
+  password: 'Wndgur52117!',
   database: 'admin',
   entities: ['dist/**/*.entity.{ts,js}'], // Entity 연결
   synchronize: false, //true 값을 설정하면 어플리케이션을 다시 실행할 때 데이터베이스가 초기화 진행
